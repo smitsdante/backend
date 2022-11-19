6a3cbe90c08b0f2be8ab56ba7729304425a16726
smitsdante <smitsdante@gmail.com>
2022-11-19 11:32:17 -0500

    agregación: se añadir los primeros archivos al repositorio

---
+++ semana-1/dia-2/1-operadores.py
@@ -0,0 +1,122 @@
+# Operadores aritmeticos
+numero1, numero2 = 10, 50 
+
+# SUMA
+# Nota: solamente sera suma si las dos variables son numericas, si es que son string entonces se hara una concatenacion y ademas no se puede sumar entre string y numeros
+print(numero1 + numero2)
+
+# RESTA
+# Nota: Solamente para numeros
+print(numero1 - numero2)
+# print('ab' - 'bc') 
+
+# MULTIPLICACION
+# Nota: si se usa la multiplicacion en un string, entonces este repetira el numero de veces entre un string y un numero de veces
+print(numero1 * numero2)
+print('hola'*5)
+
+# DIVISION
+print(numero1 / numero2)
+
+# MODULO
+print(numero1 % numero2)
+
+# COCIENTE
+print(numero1 // numero2)
+
+# EXPONENTE
+# 10^50
+print(numero1 ** numero2)
+
+# RAIZ CUADRADA usando exponente
+print(numero1 ** 0.5)
+
+
+# -----------------------------------
+# OPERADORES ASIGNACION
+# IGUAL asignar un nuevo valor a una variable
+numero1 = 100
+
+# INCREMENTO
+numero1 += 1 # incrementando el valor del numero1 en una unidad
+print(numero1)
+
+# DECREMENTO
+numero1 -= 1 # numero1 = numero1 - 1
+print(numero1)
+
+# MULTIPLICADOR
+numero1 *= 2
+print(numero1)
+
+# DIVIDENDO
+numero1 /= 5 # numero1 = numero1 / 5
+
+
+# -------------------------------------
+# OPERADORES DE COMPARACION Siempre retornaran un booleano (si es verdadero o si es falso)
+
+# IGUAL QUE
+# Nota: en python, a diferencia de JS, no existe el triple igual (comparador de tipos de datos)
+print(numero1) # Float
+print(numero2) # Int
+print(numero1 == numero2)
+print(int(40.7)) # asi se convierte el tipo de dato
+# int('eduardo') # no se puede convertir tipos de datos irreales
+print(type(numero1) == type(numero2))
+
+# MAYOR | MAYOR O IGUAL
+print(10 > 9.58)
+print(10 > int('5'))
+print(50 >= 30)
+
+# MENOR | MENOR O IGUAL
+print(50 < 80)
+print(50 <= 50)
+print(50 <= 50)
+# Nota: siempre va el simbolo de mayor(>) o menor(<) antes del igual, nunca al revez porque sino python entiende que se esta tratando de una asignacion
+
+print (100>= float("40.24"))
+
+# -----------------------------------------
+# OPERADORES LOGICOS
+# Sirve para comparar varias condicionales
+# && para indicar un AND , en python se usa la palabra AND
+# || para indicar un OR, en python se usa la palabra OR
+
+# and > TODAS las condiciones tienen que ser verdaderas para que todo sea verdadero
+eduardo=30
+ronald= 25
+henry=25
+carmen=19
+angel=15
+
+print((angel > eduardo) and (ronald < henry))
+
+
+print((eduardo > angel) and (carmen < ronald))
+
+# or > AL MENOS una condicion tiene que ser verdadera para que todo sea verdadero
+print((carmen > ronald) or (eduardo > ronald))
+
+# COMPUERTAS LOGICAS ( operador AND)
+#  V1 |  V2  |  R
+#  F  |   F  |  F
+#  V  |   F  |  F
+#  F  |   V  |  F
+#  V  |   V  |  V
+
+# COMPUERTA LOGICA (operador OR)
+#  V1 |  V2  |  R
+#  F  |   F  |  F
+#  V  |   F  |  V
+#  F  |   V  |  V
+#  V  |   V  |  V
+
+
+# -----------------------------------------
+# OPERADORES DE CONTENIDO
+verduras = ['apio', 'rocoto', 'zanahoria']
+
+print('tomate cherry' in verduras)
+print('champiñon' not in verduras)
---
+++ semana-1/dia-3/2-loops.py
@@ -0,0 +1,22 @@
+numeros = [4, 5, 8, 3, 2, 1, 9]
+
+for numero in numeros:
+  # print(numero)
+  pass
+
+alumnos = ['Jose', 'Miguel', 'Raul', 'Eduardo', 'Jorge']
+
+for i, alumno in enumerate(alumnos):
+  if i == 2:
+    # pass
+    # break
+    continue
+  # print(i, alumno)
+
+for numero in range(100):
+  # print(numero)
+  pass
+
+palabra = 'Hola mundo'
+for letra in palabra:
+  print(letra)
